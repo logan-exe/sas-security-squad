507321b6bb29fb9952a99cfdf4df1ea440d9639d
Arun Loganathan <arunloganathan@Aruns-MacBook-Pro.local>
2022-10-15 12:50:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useRef } from "react";
+import { Bar, Line, getDatasetAtEvent } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from "chart.js";
+import { Chart } from "react-chartjs-2";
+import { useState } from "react";
+import Papa from "papaparse";
+import {
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+} from "chart.js";
+import About from "./About";
+import Building from "./Building";
+import Home from "./Home";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart - Stacked",
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [100, 200, 300, 400],
+      backgroundColor: "rgb(255, 99, 132)",
+    },
+    {
+      label: "Dataset 2",
+      data: [100, 200, 300, 400],
+      backgroundColor: "rgb(75, 192, 192)",
+    },
+    {
+      label: "Dataset 3",
+      data: [100, 200, 300, 400],
+      backgroundColor: "rgb(53, 162, 235)",
+    },
+  ],
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="building" element={<Building />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Bar options={options} data={data} />; */}
+    </>
   );
 }
 
+function Users() {
+  return (
+    <div>
+      <nav>
+        <Link to="me">My Profile</Link>
+      </nav>
+
+      {/* <Routes>
+        <Route path=":id" element={<UserProfile />} />
+        <Route path="me" element={<OwnUserProfile />} />
+      </Routes> */}
+    </div>
+  );
+}
 export default App;
